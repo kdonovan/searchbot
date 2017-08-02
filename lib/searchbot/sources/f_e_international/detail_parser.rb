5e37790432f86e70eb89b74f178a10964dc38326
Kali Donovan <kali@deviantech.com>
2017-08-02 22:59:53 +0100

    fixed fe description

--- lib/searchbot/sources/f_e_international/detail_parser.rb
+++ lib/searchbot/sources/f_e_international/detail_parser.rb
@@ -8,15 +8,7 @@ class Searchbot::Sources::FEInternational::DetailParser < Searchbot::Generic::De
   end
 
   def description
-    desc = []
-    node = doc.css('h2').detect {|n| n.text == 'Description' }
-    loop do
-      node = node.next until node.nil? || node.name == 'p'
-      break unless node
-      desc << node.text
-    end
-
-    desc.join(divider)
+    (doc.css('.main-benefits li').map(&:text) + doc.at('.listing-description p').text.split('<br>')).compact.join(divider)
   end
 
   private
